8a1c16fcd92133047606dc93accaa465b005eba3
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2025-02-25 16:45:08 +0100

    Move models

---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import { Studio } from '@threlte/studio';
+	import Scene from './Scene.svelte';
+</script>
+
+<svelte:head>
+	<title>Game</title>
+	<meta name="description" content="In game" />
+</svelte:head>
+
+<div class="app">
+	<Canvas>
+		<Studio>
+			<Scene />
+		</Studio>
+	</Canvas>
+</div>
+
+<style>
+  .app {
+	display: flex;
+	min-height: 100vh;
+	min-width: 100vw;
+  }
+</style>
---
+++ src/routes/game/Scene.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { T } from '@threlte/core'
+</script>
+
+<T.Mesh position={[ -3.6, 1.7, 0.2 ]}>
+  <T.BoxGeometry />
+  <T.MeshBasicMaterial />
+</T.Mesh>
